2c3ae0c78cc58adf24011aaa87996c3fe152fccf
RomanHlodann <roman.hlodann@gmail.com>
2024-09-24 17:49:31 +0300

    add website model & migrations

--- websites/models.py
+++ websites/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Website(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="websites"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.url}"
