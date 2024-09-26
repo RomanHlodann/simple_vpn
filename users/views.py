53e408e0b84b898937d4569068c3f4181088b6bc
RomanHlodann <roman.hlodann@gmail.com>
2024-09-26 15:03:52 +0300

    remove index from users views

--- users/views.py
+++ users/views.py
@@ -1,15 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 from users.forms import RegisterForm, UserUpdateForm
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
 
 
 class UserCreationView(generic.CreateView):
@@ -22,7 +16,7 @@ class UserCreationView(generic.CreateView):
 class UserDetailView(LoginRequiredMixin, generic.DetailView):
     model = get_user_model()
     template_name = "users/detail.html"
-    queryset = get_user_model().objects.prefetch_related("tasks")
+    queryset = get_user_model().objects.all()
 
     def get_object(self, queryset=None):
         return self.request.user
