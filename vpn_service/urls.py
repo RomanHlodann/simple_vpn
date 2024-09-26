78ca76d1479b2d4e19599bafe0a0bfd3db25225c
RomanHlodann <roman.hlodann@gmail.com>
2024-09-26 15:04:12 +0300

    update urls

--- vpn_service/urls.py
+++ vpn_service/urls.py
@@ -18,10 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from users.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("users.urls")),
-    path("", index, name="index"),
+    path("vpn/", include("websites.urls")),
 ]
